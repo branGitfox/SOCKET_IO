3b8697b3d35fc9236405188f390d7ff44b5b1dad
BrandonDEv <vixfgit@gmail.com>
2024-06-30 14:08:15 +0300

    server configured

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import url from 'url'
+import {Server} from 'socket.io'
+import {createServer} from 'http'
+
+
+const app = express()
+const __dirname = dirname(url.fileURLToPath(import.meta.url))
+const server = createServer(app)
+
+const io = new Server(server)
+
+
+
+server.listen(5000, () => {
+    console.log('running on http://localhost:5000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('connected');
+})
